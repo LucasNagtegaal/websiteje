07abc80006f41ecc1a94585f3fbe34da70bbea08
Hugo <110241@st-maartenscollege.nl>
2020-01-14 09:29:44 +0100

    Show All page

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -57,6 +57,11 @@ namespace SchoolTemplate.Controllers
       return View();
     }
 
+    public IActionResult ShowAll()
+    {
+      return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
